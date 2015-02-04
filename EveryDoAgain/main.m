0498b39bffa23a7b85883507ae21eae2b7c450a0
4johnny <4johnny@users.noreply.github.com>
2015-02-03 16:56:30 -0800

    Initial Commit

---
+++ EveryDoAgain/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EveryDoAgain
+//
+//  Created by Johnny on 2015-02-03.
+//  Copyright (c) 2015 Empath Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
